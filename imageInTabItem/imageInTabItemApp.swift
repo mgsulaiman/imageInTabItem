6f863c7cc5bc02faf9e68d7f41ab06c246cd990b
mgsulaiman <info@mgsulaiman.com>
2023-04-06 05:05:30 +0300

    Initial Commit

---
+++ imageInTabItem/imageInTabItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageInTabItemApp.swift
+//  imageInTabItem
+//
+//  Created by developer mg on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct imageInTabItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
